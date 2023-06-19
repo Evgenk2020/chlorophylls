313f64b4f3da5985405a457bf31cb46328d008b2
Eugene Kopilov <64531188+Evgenk2020@users.noreply.github.com>
2023-06-19 11:37:57 +0300

    Add files via upload

--- src/output.cpp
+++ src/output.cpp
@@ -7,24 +7,25 @@ general_info::~general_info() {}
 
 void help_info::see_info()
 {
-    std::cout << "*** Определение концентрации хлорофиллов ***" << std::endl
+    std::cout << "*** Визначенні концентрації хлорофілів ***" << std::endl
               << std::endl
-              << "Для вывода в терминал" << std::endl
-              << "chloro -d [навеска г] [фильтрат мл] [проба для фотометрии мл] [растворитель для фотометрии мл] [спектрофотометр 665] [спектрофотометр 649]" << std::endl
-              << "Для вывода в файл" << std::endl
-              << "chloro -df [навеска г] [фильтрат мл] [проба для фотометрии мл] [растворитель для фотометрии мл] [спектрофотометр 665] [спектрофотометр 649]" << std::endl
-              << "Для вывода справки" << std::endl
+              << "Для виводу в термінал" << std::endl
+              << "chloro -d [наважка г] [фільтарт мл] [фотометрична_проба мл] [фотометричний_розчинник мл] [фотометр 665] [фотометр 649]" << std::endl
+              << "Для виводу в файл" << std::endl
+              << "chloro -d [наважка г] [фільтарт мл] [фотометрична_проба мл] [фотометричний_розчинник мл] [фотометр 665] [фотометр 649]" << std::endl
+              << "Для виводу довідки" << std::endl
               << "chloro [-h|--help]" << std::endl
-              << "Для просмотра дополнительной информации" << std::endl
+              << "Додаткова інформація" << std::endl
               << "chloro -i" << std::endl;
 }
 
 void inf_indo::see_info()
 {
-    std::cout << "Концентрации хлорофиллов a и b в растворах определяют по значениям экстинкции, измеренным при двух длинах волн." << std::endl
-              << "Формулы для растворов хлорофилла в 96%-ном этаноле" << std::endl
-              << "концентрация хлорофилла а (мг/мл) = 13.7 * D665 - 5.76 * D649" << std::endl
-              << "концентрация хлорофилла а (мг/мл) = 25.8 * D649 - 7.6 * D665" << std::endl;
+    std::cout << "Концентрації хлорофілів a і b в розчинах визначають відповідно величинам екстинкції, виміряних за двох довжин хвиль" << std::endl
+              << std::endl
+              << "Формули для розчинів хлорофілу в 96%-ном етанолі" << std::endl
+              << "концентрація хлорофіла а (мг/мл) = 13.7 * D665 - 5.76 * D649" << std::endl
+              << "концентрація хлорофіла b (мг/мл) = 25.8 * D649 - 7.6 * D665" << std::endl;
 }
 
 //--------------------------------------------------
@@ -35,19 +36,19 @@ void screen_info::see_info(ch_data *dat)
 {
     chlor_allowance allow;
 
-    std::cout << "Концентрация хлорофилла А: "
+    std::cout << "Концентрація хлорофілу А: "
               << allow.chloro_data_get(allow.chloro_a_allowance)->get_chloro(*dat) << " мг/мл" << std::endl;
 
-    std::cout << "Концентрация хлорофилла B: "
+    std::cout << "Концентрація хлорофілу B: "
               << allow.chloro_data_get(allow.chloro_b_allowance)->get_chloro(*dat) << " мг/мл" << std::endl;
 
-    std::cout << "Содержание хлорофилла А: "
+    std::cout << "Вміст хлорофілу А: "
               << allow.chloro_data_get(allow.chloro_a_mg)->get_chloro(*dat) << " мг/100 г" << std::endl;
 
-    std::cout << "Содержание хлорофилла B: "
+    std::cout << "Вміст хлорофілу B: "
               << allow.chloro_data_get(allow.chloro_b_mg)->get_chloro(*dat) << " мг/100 г" << std::endl;
 
-    std::cout << "Сумма хлорофиллов А + B: "
+    std::cout << "Сума хлорофілів А + B: "
               << allow.chloro_data_get(allow.chloro_sum)->get_chloro(*dat) << " мг/100 г" << std::endl;
 }
 
@@ -63,23 +64,23 @@ void file_info::see_info(ch_data *dat)
 
     chlor_allowance allow;
 
-    writer << "Концентрация хлорофилла А:" << coma
+    writer << "Концентрація хлорофілу А:" << coma
            << quo << allow.chloro_data_get(allow.chloro_a_allowance)->get_chloro(*dat) << quo << coma << quo << "мг/мл" << quo << std::endl;
 
-    writer << "Концентрация хлорофилла B:" << coma
+    writer << "Концентрація хлорофілу B:" << coma
            << quo << allow.chloro_data_get(allow.chloro_b_allowance)->get_chloro(*dat) << quo << coma << quo << "мг/мл" << quo << std::endl;
 
-    writer << "Содержание хлорофилла А:" << coma
+    writer << "Вміст хлорофілу А:" << coma
            << quo << allow.chloro_data_get(allow.chloro_a_mg)->get_chloro(*dat) << quo << coma << quo << "мг/100 г" << quo << std::endl;
 
-    writer << "Содержание хлорофилла B:" << coma
+    writer << "Вміст хлорофілу B:" << coma
            << quo << allow.chloro_data_get(allow.chloro_b_mg)->get_chloro(*dat) << quo << coma << quo << "мг/100 г" << quo << std::endl;
 
-    writer << "Сумма хлорофиллов А + B:" << coma
+    writer << "Сума хлорофілів А + B:" << coma
            << quo << allow.chloro_data_get(allow.chloro_sum)->get_chloro(*dat) << quo << coma << quo << "мг/100 г" << quo << std::endl;
     writer << "" << std::endl;
 
-    std::cout << "Данные добавлены в файл chlor-dada.csv" << std::endl;
+    std::cout << "Дані додані в файл chlor-dada.csv" << std::endl;
 }
 
 //--------------------------------------------------
